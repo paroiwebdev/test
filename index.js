a0a5cff4a7de567b76d2dc8a4a95d38684a4ceaf
Limon Paroi <34869737+paroiwebdev@users.noreply.github.com>
2019-04-17 20:46:44 +0600

    updated file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.end('hello world');
+    res.end('my first express script!');
 });
 
 app.listen(4000);
