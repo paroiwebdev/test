bb2f58ef67d2f9606996f61650bcd2c6685ba80c
Limon Paroi <34869737+paroiwebdev@users.noreply.github.com>
2019-04-18 17:52:28 +0600

    GET rndpoint updated to hello this is a test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/',(req,res)=>{
-    res.end('my first express script!');
+    res.end('hello this is a test');
 });
+
 
 app.listen(4000);
